69ad22172cd406b587a93c1992372f0ed3f6e1f0
Florian Gächter <florian.gaechter@frontify.com>
2023-05-25 16:50:43 +0200

    initial commit

---
+++ src/Block.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import type { FC } from 'react';
+import { useBlockAssets, useBlockSettings, useEditorState } from '@frontify/app-bridge';
+import { RichTextEditor, serializeRawToHtmlAsync } from '@frontify/fondue';
+import type { BlockProps } from '@frontify/guideline-blocks-settings';
+
+type Settings = {
+    myTitle: string;
+    myRichTextField: string;
+};
+
+export const AnExampleBlock: FC<BlockProps> = ({ appBridge }) => {
+    const [blockSettings, setBlockSettings] = useBlockSettings<Settings>(appBridge);
+    const { blockAssets } = useBlockAssets(appBridge);
+    const isEditing = useEditorState(appBridge);
+    const onTextChange = (value: string) =>
+        value !== blockSettings.myRichTextField && setBlockSettings({ myRichTextField: value });
+    const [html, setHtml] = useState<string | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            setHtml(await serializeRawToHtmlAsync(blockSettings.myRichTextField));
+        })();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [blockSettings.myRichTextField]);
+
+    return (
+        <div>
+            <h1>{blockSettings.myTitle}</h1>
+
+            {isEditing ? (
+                <RichTextEditor
+                    onBlur={onTextChange}
+                    onTextChange={onTextChange}
+                    placeholder="Type your text here"
+                    value={blockSettings.myRichTextField}
+                />
+            ) : (
+                <div dangerouslySetInnerHTML={{ __html: html || '<br />' }} />
+            )}
+            {blockAssets.myImage && <img src={blockAssets.myImage[0].previewUrl} alt="" />}
+        </div>
+    );
+};
